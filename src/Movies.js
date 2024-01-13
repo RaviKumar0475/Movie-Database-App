b7b2f331dddeefb76e794e5e4b5ffabae84d05f0
RaviKumar <bijjamravi662@gmail.com>
2024-01-13 13:24:15 +0000

    Movie-Database-App

---
+++ src/Movies.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import MovieInfo from './MovieInfo.js'
+// import 'react-router-dom';
+export default function Movies({title, popularity, release_date, image}) {
+  return (
+    <div className="movies-items">
+      <h1 className="title">{title}</h1>
+      <img
+        className="image"
+        src={'https://image.tmdb.org/t/p/w500/' + image}
+        alt="poster_path"
+      ></img>
+      <strong>
+        <p className="popularity">Popularity</p>
+      </strong>
+      <p>{popularity}</p>
+      <strong>
+        <p className="date">Release Date</p>
+      </strong>
+      <p>{release_date}</p>
+      <button></button>
+    </div>
+  )
+}
